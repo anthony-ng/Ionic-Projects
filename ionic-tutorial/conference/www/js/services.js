5148333ae542990b8d4b9d8e8e1638fb739a190b
anthony-ng <thomsonng123@gmail.com>
2015-02-15 14:33:39 -0800

    define a service named Session that uses the Angular resource module to provide access to the REST services at the specified endpoint

--- ionic-tutorial/conference/www/js/services.js
+++ ionic-tutorial/conference/www/js/services.js
@@ -1,2 +1,7 @@
 // define a module named starter.services with a dependency on ngResource
 angular.module('starter.services', ['ngResource'])
+
+// define a service named Session that uses the Angular resource module to provide access to the REST services at the specified endpoint
+.factory('Session', function ($resource) {
+    return $resource('http://localhost:5000/sessions/:sessionId');
+});
